ff5151fb77b2653e09ce07e2f80e86844d31e636
vishwaravi <vishwaman45@gmail.com>
2025-03-19 22:27:31 +0530

    made some changes

--- torrent-dwnloader/dwnloader.sh
+++ torrent-dwnloader/dwnloader.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-mkdir dwn
 echo -e "\e[36m Downloading Qbittorrent nox...\e[0m"
-
 sudo apt install qbittorrent-nox -y
+echo -e "\e[36m Installed qbittorrent-nox \e[0m"
 
+echo -e "\e[36m starting serveo...\e[0m"
 ssh -o StrictHostKeyChecking=no -R 80:localhost:8080 serveo.net > serveo.log &
+serveo_pid=$!
 sleep 2
+echo -e "\e[36m started serveo \e[0m"
 echo "y" | qbittorrent-nox > qbitt_out.log 2>&1 &
+qbit_pid=$! 
 sleep 1
 qbit=$(cat qbitt_out.log)
 serveo=$(cat serveo.log)
 
 echo -e "\e[36m torrent client Started...\e[0m"
-echo -e "\e[34m $serveo \e[0m"
 echo -e "\e[32m $qbit\e[0m"
+echo -e "\e[34m Running at : $serveo \e[0m"
 
 echo -e "\e[36m stop ? [enter]\e[0m" 
 read choise
 
-pkill -9 serveo
-pkill -9 qbittorrent-nox
+kill $serveo_pid
+kill $qbit_pid
+
 echo -e "\e[32m BYE...\e[0m"
