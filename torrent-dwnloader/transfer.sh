9d89cc42ab2cda5332c5da10a3cb4f7364ba4b3e
vishwaravi <vishwaman45@gmail.com>
2025-03-19 22:32:09 +0530

    made some changes

--- torrent-dwnloader/transfer.sh
+++ torrent-dwnloader/transfer.sh
@@ -3,9 +3,11 @@ mkdir -p ~/.config/rclone
 cp ~/rclone.conf ~/.config/rclone/ 
 echo -e "\e[36m rclone set finished \e[0m"
 
-if [[ $1 == "latest" ]]; then
-	cd ~/Downloads
-	latest_file=$(ls -t | awk "NR==1")
-	rclone copy "$latest_file" mydrive-one:Download/MOVIES -P
-fi
+read -p "Enter Rclone remote name (e.g., mydrive-one): " remote_name
+read -p "Enter destination folder (e.g., Download/MOVIES): " dest_folder
+
+cd ~/Downloads
+echo -e "\e[36m Transfering Last Downloaded File.. \e[0m"
+latest_file=$(ls -t | awk "NR==1")
+rclone copy "$latest_file" "$remote_name:$dest_folder" -P
 echo -e "\e[36m Completed\e[em"
